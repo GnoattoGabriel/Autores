718202bfacf0c6609a067fabab1be99b8d41da23
Gabriel Gnoatto <gnoattogabriel100@gmail.com>
2026-03-23 15:19:44 -0300

    first commit

---
+++ src/test/java/com/gnoatto/Autores/AutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnoatto.Autores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
